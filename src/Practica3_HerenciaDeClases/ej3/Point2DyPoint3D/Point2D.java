a2ee563b62361a59daa85f8763e480d555630ad8
Ventura <ventura.pacheco@edu.uah.es>
2026-03-15 23:41:55 +0100

    Añado finalemte apartado 3.1 del cuaderno 3 con sus respectivos test.

---
+++ src/Practica3_HerenciaDeClases/ej3/Point2DyPoint3D/Point2D.java
@@ -0,0 +1,69 @@
+package Practica3_HerenciaDeClases.ej3.Point2DyPoint3D;
+
+public class Point2D {
+
+    //private instance variables
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+
+    /**
+     * Constructs a Point2D instance with the given x and y coordinates
+     */
+    public Point2D(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Constructs a Point2D instance with default values
+     */
+    public Point2D(){
+        this.x = 0.0f;
+        this.y = 0.0f;
+    }
+
+    //Getter for instance variable x
+    public float getX(){
+        return x;
+    }
+
+    //Setter for instance variable x
+    public void setX(float x){
+        this.x = x;
+    }
+
+    //Getter for instance variable y
+    public float getY(){
+        return y;
+    }
+
+    //Setter for instance variable y
+    public void setY(float y){
+        this.y = y;
+    }
+
+    /**
+     * Sets x and y coordinates
+     */
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns a float array of {x, y}
+     */
+    public float[] getXY(){
+        return new float[]{getX(),getY()};
+    }
+
+    /**
+     * Returns a self-descriptive string in the form of (x,y)
+     */
+    @Override
+    public String toString(){
+        return "("+ x + ","+ y +")";
+    }
+}
+
---
+++ src/Practica3_HerenciaDeClases/ej3/Point2DyPoint3D/Point3D.java
@@ -0,0 +1,56 @@
+package Practica3_HerenciaDeClases.ej3.Point2DyPoint3D;
+
+public class Point3D extends Point2D {
+
+    //private instance variable
+    private float z = 0.0f;
+
+    /**
+     * Constructs a Point3D instance with the given x, y and z coordinates
+     */
+    public Point3D(float x, float y, float z){
+        super(x,y);
+        this.z = z;
+    }
+
+    /**
+     * Constructs a Point3D instance with default values
+     */
+    public Point3D(){
+        super();
+        this.z = 0.0f;
+    }
+
+    //Getter for instance variable z
+    public float getZ(){
+        return z;
+    }
+
+    //Setter for instance variable z
+    public void setZ(float z){
+        this.z = z;
+    }
+
+    /**
+     * Sets x, y and z coordinates
+     */
+    public void setXYZ(float x, float y, float z){
+        super.setXY(x,y);
+        this.z = z;
+    }
+
+    /**
+     * Returns a float array of {x, y, z}
+     */
+    public float[] getXYZ(){
+        return new float[]{getX(),getY(),getZ()};
+    }
+
+    /**
+     * Returns a self-descriptive string in the form of (x,y,z)
+     */
+    @Override
+    public String toString(){
+        return "(" + getX() + "," + getY() +"," + z + ")";
+    }
+}
