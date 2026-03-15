858e331bd550a85a488192056d926f65d5e1a5f8
Marino <marino.rodriguezm@edu.uah.es>
2026-03-15 17:33:42 +0100

    Añado el apartado 2.9 de la segunda practica con sus respectivos tests

--- test/java/Practica2_ComposicionAgregacion/ej8/MyCircle/MyCircleTest.java
+++ test/java/Practica2_ComposicionAgregacion/ej8/MyCircle/MyCircleTest.java
@@ -14,7 +14,7 @@ class MyCircleTest {
         assertEquals(1, c0.getRadius());
 
         // Test constructor con coordenadas int y radio int
-        MyCircle c1 = new MyCircle(6,6,3);
+        MyCircle c1 = new MyCircle(6, 6, 3);
         assertEquals(6, c1.getCenterX());
         assertEquals(6, c1.getCenterY());
 
@@ -27,13 +27,13 @@ class MyCircleTest {
         MyPoint p3 = new MyPoint(10, 10);
         c2.setCenter(p3);
         assertEquals(10, c2.getCenterX());
-        c2.setCenterXY(12,14);
-        assertArrayEquals(new int[]{12, 14},c2.getCenterXY());
+        c2.setCenterXY(12, 14);
+        assertArrayEquals(new int[]{12, 14}, c2.getCenterXY());
         c2.setRadius(4);
         assertEquals(4, c2.getRadius());
         c2.setCenterX(13);
         c2.setCenterY(9);
-        assertArrayEquals(new int[]{13, 9},c2.getCenterXY());
+        assertArrayEquals(new int[]{13, 9}, c2.getCenterXY());
 
     }
 
@@ -51,14 +51,14 @@ class MyCircleTest {
 
     @Test
     void testDistance() {
-        MyCircle c1 = new MyCircle(5,5,1);
-        MyCircle c2 = new MyCircle(7,7,1);
+        MyCircle c1 = new MyCircle(5, 5, 1);
+        MyCircle c2 = new MyCircle(7, 7, 1);
         assertEquals(Math.sqrt(8), c1.distance(c2), 0.001);
     }
 
     @Test
     void testToString() {
-        MyCircle c = new MyCircle(3,5,8);
+        MyCircle c = new MyCircle(3, 5, 8);
         assertEquals("MyCircle[radius=8,center=(3,5)]", c.toString());
     }
 }
