243d5d9861d5a8e64e6fb700fd9deaccc4df906d
Ventura <ventura.pacheco@edu.uah.es>
2026-03-15 12:38:00 +0100

    Prueba cambio de nombre

--- src/Practica2_ComposicionAgregacion/ej8/MyCircle/MyCircle.java
+++ src/Practica2_ComposicionAgregacion/ej8/MyCircle/MyCircle.java
@@ -1,5 +1,7 @@
 package Practica2_ComposicionAgregacion.ej8.MyCircle;
 
+import Practica2_ComposicionAgregacion.ej6.MyPoint.MyPoint;
+
 public class MyCircle {
-vevaercas
+    private MyPoint centre;
 }
